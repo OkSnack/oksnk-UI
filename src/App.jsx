be7ebd3e3c68680bf3a821ea5baf5c6f0981877b
C1ure <carlos1403iure@gmail.com>
2025-02-04 07:19:28 -0300

    [ADD] Arquives files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 
-import {BoxLine,SubTitle,Title,BreakLine, BoxItem} from './style/style-global'
+import {BoxLine,SubTitle,Title,BreakLine, BoxItem, Btn, TextBtn} from './style/style-global'
 import color from './style/colors'
-
+import './pages/Desc'
 function App() {
   return (
     <>
       <BoxLine bg_color={color.gray2} height='100vh' width='100vw' direction='column'>
-        <SubTitle size='25px' color={color.white} weight='bold'> Teste de projeto</SubTitle>
+          <SubTitle size='25px' color={color.white} weight='bold'> Teste de projeto</SubTitle>
          <BoxItem height='60px'>
-        <Title size='55px' color={color.success} weight='bold'> Ok Snack UI</Title>
-
+          <Title size='55px' color={color.success} weight='bold'> Ok Snack UI</Title>
          </BoxItem>
+         <BoxItem height='50px' margin='10px 0px'>
+          <Btn width='20% - 16px' hoverBg={color.success} onClick={() => {}}> 
+            <TextBtn color={color.black} hoverColor={color.white} >  Ver mais </TextBtn>
+          </Btn>
+         </BoxItem>
+       
+        
       </BoxLine>
     </>
   )
